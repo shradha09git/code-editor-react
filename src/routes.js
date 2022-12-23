12f0cc86610a711c20c3818af38adc45224de699
Shradha_Andhare <73115655+shradha09git@users.noreply.github.com>
2022-12-23 18:27:08 +0530

    Create routes.js

---
+++ src/routes.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Home = React.lazy(() => import("./Pages/Home"));
+const PlayGround = React.lazy(() => import("./Pages/PlayGround"));
+const Page404 = React.lazy(() => import("./Pages/Page404"));
+
+ const routes = [
+    {
+        path: "/",
+        component: <Home />
+    },
+    {
+        path:"/playground/:folderId/:playgroundId" ,
+        component: <PlayGround />
+    },
+    {
+        path: "*",
+        component: <Page404 />
+    }
+
+];
+export default routes;
